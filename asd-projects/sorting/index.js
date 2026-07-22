a84961f54064a861139c007c233028da9433cd92
calebriordan1989 <calebriordan8097@gmail.com>
2026-07-22 19:20:30 +0000

    saving sorting exercise

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -14,15 +14,84 @@ The CSS ids you will work with are:
 ///////////////////////////////////////////////////////////////////////
 
 // TODO 2: Implement bubbleSort
-
+//Created function bubbleSort with 1 parameter and also placed async in front. 
+async function bubbleSort(array) {
+    //This is going to be the first iteration found in the pseudocode.
+    for (var i = 0; i <= array.length-1; i++) {
+        //This is going to be the second iteration found in the pseudocode.
+        for (var j = array.length - 1;j >= i + 1; j--) {
+            //Here is the if statement that will compare two values and swap them if necessary. 
+            if (array[j].value < array[j - 1].value) {
+                swap (array, j, j -1);
+            //This is 2c that was provided in the instructions. 
+            updateCounter(bubbleCounter);
+            await sleep();
+            }
+            
+        }
+    }
+    
+}
 
 // TODO 3: Implement quickSort
+//Created function quickSort with 3 parameters and also placed async in front.
+async function quickSort(array, left, right) {
+    //The next 3 if statements follow along with the pseudocode provided in the directions. 
+    if (right - left > 0) {
+       var index = await partition(array, left, right); 
+    
+    if (left < index - 1) {
+       await quickSort(array, left, index - 1)
+    } 
+    
+    if (index < right){
+        await quickSort(array, index, right)
+        }
+    }
+
+}
 
 
 // TODOs 4 & 5: Implement partition
+async function partition(array, left, right) {
+    //This will select the middle element value as the pivot point. 
+    var pivot = array[Math.floor((right + left) / 2)].value;
+    //This loop will run as long as left is less than right. 
+    while (left <= right){
+        //This will increment left until the pivot is found.
+       while (array[left].value < pivot) {
+        left++;
+       }
+        while (array[right].value > pivot) {
+            //This will decrement right until the pivot is found. 
+            right--;
+        }
+        if (left <= right) {
+            //This will swap elements if the left is less than the right. 
+            swap(array, left, right);
+            updateCounter(quickCounter);
+            await sleep();
+            left++;
+            right--;
+        }
+        }
+    return left
+}
 
 
 // TODO 1: Implement swap
+//The function will be created here and have three parameters per the instructions.
+function swap (array, i, j) {
+    //This will create a variable called temporary. At first it is set equal to i, then i is set equal to j, and finally j is set equal to the temporary variable. 
+    //This should allow for the indexes to be swapped. 
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+
+    //This is calling the helper function drawSwap with the three parameters also found in swap function. 
+    drawSwap(array, i, j);
+}
+
 
 
 ///////////////////////////////////////////////////////////////////////
